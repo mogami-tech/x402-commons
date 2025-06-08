aafac1e2d605aa7620acdd68f4ae9e2eb49fe7f6
straumat <github.backlog426@passmail.net>
2025-06-08 13:48:05 +0200

    Add test for SchemeValidator

---
+++ src/test/java/tech/mogami/commons/test/SchemeValidatorTest.java
@@ -0,0 +1,38 @@
+package tech.mogami.commons.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tech.mogami.commons.validator.SchemeValidator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SchemeValidatorTest {
+
+    private SchemeValidator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new SchemeValidator();
+    }
+
+    @Test
+    void shouldReturnTrueForNullScheme() {
+        assertThat(validator.isValid(null, null)).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrueForEmptyScheme() {
+        assertThat(validator.isValid("", null)).isTrue();
+    }
+
+    @Test
+    void shouldReturnFalseForUnknownScheme() {
+        assertThat(validator.isValid("INVALID_SCHEME", null)).isFalse();
+    }
+
+    @Test
+    void shouldReturnTrueForKnownScheme() {
+        assertThat(validator.isValid("exact", null)).isTrue();
+    }
+
+}
