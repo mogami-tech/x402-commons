995192e5298487bf86415c7b570be1da73c537a0
straumat <stephane.traumat@mogami.tech>
2025-10-15 22:21:58 +0200

    Added constants for contracts

---
+++ src/main/java/tech/mogami/commons/constant/network/base/BaseContracts.java
@@ -0,0 +1,18 @@
+package tech.mogami.commons.constant.network.base;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Base network contracts.
+ */
+@UtilityClass
+@SuppressWarnings({"checkstyle:HideUtilityClassConstructor", "unused"})
+public class BaseContracts {
+
+    /** Base sepolia USDC contract address. */
+    public static final String BASE_SEPOLIA_USDC_CONTRACT = "0x036CbD53842c5426634e7929541eC2318f3dCF7e";
+
+    /** Base mainnet USD Coin contract address. */
+    public static final String BASE_MAINNET_USDC_CONTRACT = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
+
+}
--- src/main/java/tech/mogami/commons/header/payment/schemes/exact/ExactSchemeConstants.java
+++ src/main/java/tech/mogami/commons/header/payment/schemes/exact/ExactSchemeConstants.java
@@ -9,6 +9,9 @@ import lombok.experimental.UtilityClass;
 @SuppressWarnings({"checkstyle:HideUtilityClassConstructor", "unused"})
 public class ExactSchemeConstants {
 
+    /** Exact scheme name. */
+    public static final String EXACT_SCHEME_NAME = "exact";
+
     /** Exact scheme parameter name. */
     public static final String EXACT_SCHEME_PARAMETER_NAME = "name";
 
