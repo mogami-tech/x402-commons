618b96a1af4e16db3b33ec062d64b5beb9b4f00d
straumat <stephane.traumat@mogami.tech>
2025-10-24 16:35:38 +0200

    Add a class with global model attributes

---
+++ src/main/java/tech/mogami/commons/web/GlobalModelAttributes.java
@@ -0,0 +1,33 @@
+package tech.mogami.commons.web;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Global model attributes.
+ */
+@UtilityClass
+@SuppressWarnings({"checkstyle:HideUtilityClassConstructor", "unused"})
+public class GlobalModelAttributes {
+
+    /** Error message. */
+    public static final String ERROR_MESSAGE_ATTRIBUTE = "errorMessage";
+
+    /** Query attribute. */
+    public static final String QUERY_ATTRIBUTE = "query";
+
+    /** Result attribute. */
+    public static final String RESULT_ATTRIBUTE = "result";
+
+    /** Form attribute. */
+    public static final String FORM_ATTRIBUTE = "command";
+
+    /** Page title attribute. */
+    public static final String PAGE_TITLE_ATTRIBUTE = "pageTitle";
+
+    /** Page description attribute. */
+    public static final String PAGE_DESCRIPTION_ATTRIBUTE = "pageDescription";
+
+    /** Payment nonce attribute. */
+    public static final String PAYMENT_NONCE_ATTRIBUTE = "nonce";
+
+}
