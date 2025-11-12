e1833be4642c5189344b15dd0cffcd1c23987e69
straumat <stephane.traumat@mogami.tech>
2025-11-12 14:54:44 +0100

    Added constants to display shorten address

--- src/main/java/tech/mogami/commons/constant/BlockchainConstants.java
+++ src/main/java/tech/mogami/commons/constant/BlockchainConstants.java
@@ -15,6 +15,12 @@ public class BlockchainConstants {
     /** Blockchain address length. */
     public static final int BLOCKCHAIN_ADDRESS_LENGTH = 42;
 
+    /** Blockchain shorten prefix length. */
+    public static final int BLOCKCHAIN_ADDRESS_SHORTEN_PREFIX_LENGTH = 5;
+
+    /** Blockchain shorten suffix length. */
+    public static final int BLOCKCHAIN_ADDRESS_SHORTEN_SUFFIX_LENGTH = 3;
+
     /** Blockchain address prefix. */
     public static final String BLOCKCHAIN_ADDRESS_PREFIX = "0x";
 
